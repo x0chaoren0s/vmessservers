f23b11cf78cd6ff9f3667f53f2e5f5f14059d2c9
x0chaoren0s <x0chaoren0s@gmail.com>
2023-06-11 20:32:53 +0800

    update

--- run.py
+++ run.py
@@ -4,13 +4,15 @@ from spider.server.server_parser_lionssh import SP_LIONSSH
 from spider.server.server_parser_greenssh import SP_GREENSSH
 from spider.server.server_parser_sshvpnfree import SP_SSHVPNFREE
 from spider.server.server_parser_vpnjantit import SP_VPNJANTIT
+from spider.server.server_parser_sshocean import SP_SSHOCEAN
+from spider.server.server_parser_freevmess import SP_FREEVMESS
 from utils.update_subscription import update_subscription
 
 import os
 
 if __name__ == '__main__':
-    # spiders = [SP_lionssh,SP_FREEVMESS]
-    spiders = [SP_VPNJANTIT,]
+    spiders = [SP_LIONSSH,SP_GREENSSH,SP_SSHVPNFREE,SP_VPNJANTIT,SP_SSHOCEAN,SP_FREEVMESS]
+    spiders = [SP_FREEVMESS,]
     spider_processes = [Process(target=s.parse) for s in spiders]
     [process.start() for process in spider_processes]
     [process.join() for process in spider_processes]
--- test.py
+++ test.py
@@ -1,7 +1,20 @@
 from spider.server_list.server_list_parser_vpnjantit import SLP_VPNJANTIT
 from spider.server_list.server_list_parser_lionssh import SLP_LIONSSH
+from spider.server_list.server_list_parser_akunssh import SLP_AKUNSSH
+from spider.server_list.server_list_parser_sshvpnfree import SLP_SSHVPNFREE
+from spider.server_list.server_list_parser_sshocean import SLP_SSHOCEAN
+from spider.server_list.server_list_parser_freevmess import SLP_FREEVMESS
+from spider.server.server_parser_sshvpnfree import SP_SSHVPNFREE, Server_parser_sshvpnfree
 # from spider.server.server_parser_vpnjantit import Server_parser_vpnjantit
 # from spider.server_list.server_list_parser_serverssh import SLP_SERVERSSH
 
-# SLP_SSHOCEAN.parse()
-print(SLP_LIONSSH.parse())
+# print(SLP_LIONSSH.parse())
+sl=SLP_FREEVMESS.parse()
+for item in list(sl.items()):
+    print(item)
+print(len(sl))
+# server_dict={
+#     'https://sshvpnfree.com/accounts/VMESS/60': {'region': 'France, Gravelines', 'port': 80, 'Referer': 'https://sshvpnfree.com/type/VMESS'}
+# }
+# SP=Server_parser_sshvpnfree(server_dict=server_dict)
+# SP.parse()
