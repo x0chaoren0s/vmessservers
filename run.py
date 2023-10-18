fc92ac96b54f3efdd4aed5f259f3378f8ca98cd2
x0chaoren0s <x0chaoren0s@gmail.com>
2023-10-18 20:27:35 +0800

    spider_update

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     # spiders = [SP_SSHOCEAN,SP_GREENSSH,SP_FREEVMESS]
     # spiders = [SP_GREENSSH,SP_VPNJANTIT,SP_SSHOCEAN]
     # spiders = []
-    spiders = [SP_VPNJANTIT]
+    # spiders = [SP_VPNJANTIT]
     spider_processes = [Process(target=s.parse) for s in spiders]
     [process.start() for process in spider_processes]
     [process.join() for process in spider_processes]
