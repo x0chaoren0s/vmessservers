008d5b02a4e239d4df4f7cd87c492fe226ce9419
x0chaoren0s <x0chaoren0s@gmail.com>
2024-02-29 12:16:02 +0800

    spider_update

--- test.py
+++ test.py
@@ -12,12 +12,12 @@ from spider.server.server_parser_lionssh import Server_parser_lionssh
 # from spider.server_list.server_list_parser_serverssh import SLP_SERVERSSH
 
 # print(SLP_AKUNSSH.parse())
-# sl=SLP_LIONSSH.parse()
-# for item in list(sl.items()):
-#     print(item)
+sl=SLP_FREEVMESS.parse()
+for item in list(sl.items()):
+    print(item)
 # print(len(sl))
-server_dict={
-    'https://lionssh.com/create/vmess-ws-id-1': {'region': 'Indonesia', 'host': 'vm.id.servergo.pw', 'port': 80, 'Referer': 'https://lionssh.com/services/vray/asia/ID/7-days'}
-}
-SP=Server_parser_lionssh(server_dict=server_dict)
-SP.parse()
+# server_dict={
+#     'https://lionssh.com/create/vmess-ws-id-1': {'region': 'Indonesia', 'host': 'vm.id.servergo.pw', 'port': 80, 'Referer': 'https://lionssh.com/services/vray/asia/ID/7-days'}
+# }
+# SP=Server_parser_lionssh(server_dict=server_dict)
+# SP.parse()
