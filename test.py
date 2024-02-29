7b4e659c147f78b5c4d098ca8eb80e3efa549efe
x0chaoren0s <x0chaoren0s@gmail.com>
2024-02-29 11:40:33 +0800

    spider_update

--- test.py
+++ test.py
@@ -8,15 +8,16 @@ from spider.server_list.server_list_parser_opentunnel import SLP_OPENTUNNEL
 from spider.server.server_parser_sshvpnfree import SP_SSHVPNFREE, Server_parser_sshvpnfree
 from spider.server.server_parser_vpnjantit import Server_parser_vpnjantit
 from spider.server.server_parser_sshocean import Server_parser_sshocean
+from spider.server.server_parser_lionssh import Server_parser_lionssh
 # from spider.server_list.server_list_parser_serverssh import SLP_SERVERSSH
 
 # print(SLP_AKUNSSH.parse())
-sl=SLP_VPNJANTIT.parse()
-for item in list(sl.items()):
-    print(item)
+# sl=SLP_LIONSSH.parse()
+# for item in list(sl.items()):
+#     print(item)
 # print(len(sl))
-# server_dict={
-#     'https://sshocean.com/v2ray/vmess/canada/ca1': {'region': 'Canada', 'host': 'ca1.xv2ray.net', 'cloudflare_host': 'ca1.xv2ray.net', 'port': 80, 'Referer': 'https://sshocean.com/v2ray/vmess/canada'}
-# }
-# SP=Server_parser_sshocean(server_dict=server_dict)
-# SP.parse()
+server_dict={
+    'https://lionssh.com/create/vmess-ws-id-1': {'region': 'Indonesia', 'host': 'vm.id.servergo.pw', 'port': 80, 'Referer': 'https://lionssh.com/services/vray/asia/ID/7-days'}
+}
+SP=Server_parser_lionssh(server_dict=server_dict)
+SP.parse()
