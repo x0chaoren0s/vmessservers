9ed5123c00b980865692f97646401d28399ea203
x0chaoren0s <x0chaoren0s@gmail.com>
2024-05-31 22:31:18 +0800

    精简

--- tmp.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-driver = webdriver.Edge()      # Edge浏览器
-
-# 打开网页
-url='https://zhuanlan.zhihu.com/p/111859925'
-driver.get(url) # 打开url网页 比如 driver.get("http://www.baidu.com")
-driver.find_element(By.XPATH, )
