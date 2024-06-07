47180739728327fc41da2f04f00830b727e9548f
x0chaoren0s <x0chaoren0s@gmail.com>
2024-06-07 23:44:44 +0800

    chores

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ from spider.server.server_parser_sshocean import SP_SSHOCEAN
 from spider.server.server_parser_freevmess import SP_FREEVMESS
 from spider.server.server_parser_akunssh import SP_AKUNSSH
 
-import os
+import os,time
 
 if __name__ == '__main__':
     spiders = [SP_LIONSSH,SP_GREENSSH,SP_VPNJANTIT,SP_SSHOCEAN,SP_FREEVMESS,SP_AKUNSSH]
@@ -20,7 +20,10 @@ if __name__ == '__main__':
     # spiders = []
     # spiders = [SP_GREENSSH,SP_VPNJANTIT,SP_FREEVMESS,SP_AKUNSSH]
     spider_processes = [Process(target=s.run) for s in spiders]
-    [process.start() for process in spider_processes]
+    # [process.start() for process in spider_processes]
+    for process in spider_processes:
+        process.start()
+        time.sleep(5)
     [process.join() for process in spider_processes]
     # SP_LIONSSH.run()
 
