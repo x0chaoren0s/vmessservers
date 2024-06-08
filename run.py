8d2b25805fc678ce2806d47635a807fe505eeee2
x0chaoren0s <x0chaoren0s@gmail.com>
2024-06-09 00:09:23 +0800

    spider_update

--- run.py
+++ run.py
@@ -11,21 +11,22 @@ from spider.server.server_parser_akunssh import SP_AKUNSSH
 import os,time
 
 if __name__ == '__main__':
+    # SP_GREENSSH 的网站和服务器的ip似乎都不通
     spiders = [SP_LIONSSH,SP_GREENSSH,SP_VPNJANTIT,SP_SSHOCEAN,SP_FREEVMESS,SP_AKUNSSH]
     # spiders = [SP_GREENSSH,SP_VPNJANTIT,SP_SSHOCEAN,SP_FREEVMESS,SP_AKUNSSH]
     # spiders = [SP_LIONSSH,SP_SSHVPNFREE,SP_VPNJANTIT,SP_SSHOCEAN,]
     # spiders = [SP_LIONSSH,SP_GREENSSH,SP_SSHVPNFREE,SP_SSHOCEAN,SP_FREEVMESS]
-    spiders = [SP_GREENSSH,SP_VPNJANTIT]
+    spiders = [SP_GREENSSH]
     # spiders = [SP_LIONSSH,]
     # spiders = []
     # spiders = [SP_GREENSSH,SP_VPNJANTIT,SP_FREEVMESS,SP_AKUNSSH]
-    spider_processes = [Process(target=s.run) for s in spiders]
-    # [process.start() for process in spider_processes]
-    for process in spider_processes:
-        process.start()
-        time.sleep(5)
-    [process.join() for process in spider_processes]
-    # SP_LIONSSH.run()
+    # spider_processes = [Process(target=s.run) for s in spiders]
+    # # [process.start() for process in spider_processes]
+    # for process in spider_processes:
+    #     process.start()
+    #     time.sleep(5)
+    # [process.join() for process in spider_processes]
+    SP_VPNJANTIT.run()
 
     if os.environ.get('MODE', 'RELEASE')=='DEBUG':
         from utils.update_subscription import update_subscription
