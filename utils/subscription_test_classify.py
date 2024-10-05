44811bfce89076751415c5a30e9009bba14df25f
x0chaoren0s <x0chaoren0s@google.com>
2024-10-05 12:36:34 +0800

    debug

--- utils/subscription_test_classify.py
+++ utils/subscription_test_classify.py
@@ -271,8 +271,8 @@ async def async_test_one_link(link):
     try:  
         link_json = decode_raw_link(link)  
         assert link_json['protocol'] in ['vmess', 'vless', 'trojan'], "protocal not in ['vmess', 'vless', 'trojan']"
-        assert 'add' in link_json in ['vmess', 'vless', 'trojan'], "add not in link_json"
-        assert 'port' in link_json in ['vmess', 'vless', 'trojan'], "port not in link_json"
+        assert 'add' in link_json, "add not in link_json"
+        assert 'port' in link_json, "port not in link_json"
         if link_json['protocol'] in ['vmess', 'vless', 'trojan']:
             assert tcp_ping(link_json['add'], link_json['port']), 'Tcp_Ping_Error'
         port = find_available_port()  
